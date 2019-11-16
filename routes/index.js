c86fb465fd3736f6ef021bc3da85f2f62975902c
uday <uday.pb26@gmail.com>
2019-11-15 17:07:22 -0700

    first commit'

---
+++ routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var {Parser}=require('json2csv');
+var fs=require('fs');
+var spawn=require('child_process').spawn;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res, next) {
+  let data=req.body
+    console.log(data);
+    let header=`'score_overall', 'nose_score', 'nose_x', 'nose_y', 'leftEye_score',
+    'leftEye_x', 'leftEye_y', 'rightEye_score', 'rightEye_x', 'rightEye_y',
+    'leftEar_score', 'leftEar_x', 'leftEar_y', 'rightEar_score',
+    'rightEar_x', 'rightEar_y', 'leftShoulder_score', 'leftShoulder_x',
+    'leftShoulder_y', 'rightShoulder_score', 'rightShoulder_x',
+    'rightShoulder_y', 'leftElbow_score', 'leftElbow_x', 'leftElbow_y',
+    'rightElbow_score', 'rightElbow_x', 'rightElbow_y', 'leftWrist_score',
+    'leftWrist_x', 'leftWrist_y', 'rightWrist_score', 'rightWrist_x',
+    'rightWrist_y', 'leftHip_score', 'leftHip_x', 'leftHip_y',
+    'rightHip_score', 'rightHip_x', 'rightHip_y', 'leftKnee_score',
+    'leftKnee_x', 'leftKnee_y', 'rightKnee_score', 'rightKnee_x',
+    'rightKnee_y', 'leftAnkle_score', 'leftAnkle_x', 'leftAnkle_y',
+    'rightAnkle_score', 'rightAnkle_x', 'rightAnkle_y'`;
+
+    let csv ='';
+    csv+=header.replace(/(?:\r\n|\r|\n)/g, '').replace(/\s/g, "");;
+    csv+='\n,';
+    data.map(function(d){
+      csv=csv.slice(0,csv.length-2);
+      csv+='\n';
+      csv+=`${d.score},`;
+      let frameKeypoints=d.keypoints;
+      let keyObj={}
+      frameKeypoints.map(function(k){
+        keyObj[k.part+'_score']=k.score;
+        keyObj[k.part+'_x']=k.position.x;
+        keyObj[k.part+'_y']=k.position.y;
+      });
+      let st=header.split(',');
+      st=st.slice(1)
+      // console.log('st is ',st)
+      // console.log(keyObj)
+      st.forEach(function(s){
+        let str=s.replace(/\\"/g, '"');
+        str=str.trimStart();
+        str=str.trimEnd();
+        str = str.slice(1, -1);
+        // console.log('str is ',str)
+
+        csv+=`${keyObj[str]},`;
+      });
+      
+    });
+    fs.writeFile('video.csv', csv.slice(0,csv.length-2), (err) => {
+      // throws an error, you could also catch it here
+      if (err) throw err;
+      // fs.end();
+      // success case, the file was saved
+      console.log('csv saved!');
+
+      runModel('');
+  });
+
+});
+
+function runModel(path){
+var process=spawn('python',['C:/Users/smpsh/Desktop/gestureClassify/untitled3.py']);
+console.log('process called')
+var tostr=function(d){
+  return String.fromCharCode.apply(null,d);
+}
+process.stdout.on('data',function(d){
+ d= tostr(d);
+  console.log('got back data')
+  console.log(d)
+});
+process.stderr.on('d',function(d){
+  console.log(d)
+})
+
+process.on('exit',function(d){
+  console.log(d)
+  console.log()
+})
+
+}
+module.exports = router;
---
+++ untitled3.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 22:07:39 2019
+
+@author: smpsh
+"""
+
+import numpy as np
+import pandas as pd 
+from sklearn.externals import joblib
+
+
+
+new_data = pd.read_csv("video.csv")
+model_1 = joblib.load("rfee.pkl")
+model_2 = joblib.load("dcee.pkl")
+#model_3 = joblib.load("xgb.pkl")
+encoder = joblib.load("enc.pkl")
+prediction = model_1.predict(new_data)
+prediction_category = pd.DataFrame(encoder.inverse_transform(prediction),columns = ['class'])
+value_count = pd.DataFrame(prediction_category['class'].value_counts(),columns = ['class'])
+result_1 = list(value_count[value_count['class']==value_count['class'].max()].index)[0]
+
+print (value_count)
+
+prediction_2 = model_2.predict(new_data)
+prediction_category_2 = pd.DataFrame(encoder.inverse_transform(prediction_2),columns = ['class'])
+value_count_2 = pd.DataFrame(prediction_category_2['class'].value_counts(),columns = ['class'])
+result_2 = list(value_count_2[value_count_2['class']==value_count_2['class'].max()].index)[0]
+print(value_count_2)
+print(result_1,result_2)
+
+
+
+#
+#a = pd.concat([mmm,pd.DataFrame([90],index = ['buy'])],axis = 0)
+#z = prediction_category['class'].append(pd.DataFrame(['buy']*100))
+#prediction_category['class'].max()
+#
+#aa = a[a[0]==a[0].max()]
+#aaa = list(aa.index)
